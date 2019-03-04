003e3a6cb9f82a70f48a9144b53614efc1b9beaa
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-03-03 21:30:07 -0800

    Choose some fonts to start with

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,10 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    googleFonts: [
+      'Crimson+Text:400,700',
+      'Yantramanav:400,700'
+    ],
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
