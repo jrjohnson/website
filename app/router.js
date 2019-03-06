d4fa4b9b67c3428682aa82406c643320c5ed6850
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-03-06 15:49:19 -0800

    Starting to add some navigation and empty routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('posts');
+  this.route('talks');
+  this.route('resume');
+  this.route('projects');
 });
 
 export default Router;
