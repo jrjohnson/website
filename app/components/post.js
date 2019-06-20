285f72c503dc73bf61e2edb76631d546729e04cd
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-06-19 22:13:17 -0700

    Highlight talk text the same as posts

--- app/components/post.js
+++ app/components/post.js
@@ -3,9 +3,13 @@ import hljs from 'highlight.js/lib/highlight';
 import bash from 'highlight.js/lib/languages/bash';
 import javascript from 'highlight.js/lib/languages/javascript';
 import php from 'highlight.js/lib/languages/php';
+import yaml from 'highlight.js/lib/languages/yaml';
+import dockerfile from 'highlight.js/lib/languages/dockerfile';
 hljs.registerLanguage('bash', bash);
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('php', php);
+hljs.registerLanguage('yaml', yaml);
+hljs.registerLanguage('dockerfile', dockerfile);
 
 export default Component.extend({
   didInsertElement() {
