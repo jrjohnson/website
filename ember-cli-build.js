e9dc14c684bf988b84c9dba54f0ef9fed49b13c2
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-07-21 15:34:01 -0700

    Remove postcss
    Going back SASS, too much work to maintain PostCSS when this is the only place I'm using it.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,23 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    postcssOptions: {
-      compile: {
-        enabled: true,
-        plugins: [
-          { module: require('postcss-import') },
-          { module: require('postcss-custom-media') },
-          { module: require('postcss-nesting') },
-          { module: require('cssstats') },
-          { module: require('postcss-stats-reporter') },
-          { module: require('postcss-reporter') },
-        ],
-      },
-      filter: {
-        enabled: true,
-        plugins: [{ module: require('autoprefixer') }],
-      },
-    },
   });
 
   app.import('node_modules/highlight.js/styles/a11y-dark.css');
