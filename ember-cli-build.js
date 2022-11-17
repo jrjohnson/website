b1ce1a138a3ea03fe6474ec5010ba362183e4cdb
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-11-16 23:42:16 -0800

    Update Ember CLI to v4.8.0
    and all blueprints

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,9 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {});
+  const app = new EmberApp(defaults, {
+    // Add options here
+  });
 
   app.import('node_modules/highlight.js/styles/a11y-dark.css');
 
