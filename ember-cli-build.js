4d9088113b9f5126d18645aa21aef73051d8a49b
Jonathan Johnson <jon.johnson@ucsf.edu>
2023-06-01 05:35:12 -0700

    Update Ember CLI v4.8.0...v4.12.1
    With all blueprints, and removed array prototype extensions now that
    they are deprecated.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,8 +6,6 @@ module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
   });
-
   app.import('node_modules/highlight.js/styles/a11y-dark.css');
-
   return app.toTree();
 };
