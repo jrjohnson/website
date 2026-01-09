c397cc1c749f466763995f368a63e27bb4c14197
Jonathan Johnson <jon.johnson@ucsf.edu>
2026-01-08 21:26:19 -0800

    Improve Test Config
    Now we build first and then test and have playwright run it's own
    server.

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,8 +21,6 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -71,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm run preview',
+    url: 'http://localhost:4321',
+    reuseExistingServer: !process.env.CI,
+  },
 });
